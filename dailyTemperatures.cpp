1f61708d35eb633a64430409cab0a5151e1b81e0
milos-novakovic <58906634+milos-novakovic@users.noreply.github.com>
2020-08-11 14:34:04 +0200

    Update dailyTemperatures.cpp

--- dailyTemperatures.cpp
+++ dailyTemperatures.cpp
@@ -14,14 +14,15 @@ Here is a worked example of the contents of the stack as we work through T = [73
  in reverse order, at the end of the loop (after we add T[i]). 
  For clarity, stack only contains indices i, but we will write the value of T[i] beside it in brackets, such as 0 (73).
 
-When i = 7, stack = [7 (73)]. 							ans[7] = 0.
-When i = 6, stack = [6 (76)]. 							ans[6] = 0.
-When i = 5, stack = [5 (72), 6 (76)]. 					ans[5] = 1.
-When i = 4, stack = [4 (69), 5 (72), 6 (76)]. 			ans[4] = 1.
-When i = 3, stack = [3 (71), 5 (72), 6 (76)]. 			ans[3] = 2.
-When i = 2, stack = [2 (75), 6 (76)]. 					ans[2] = 4.
-When i = 1, stack = [1 (74), 2 (75), 6 (76)]. 			ans[1] = 1.
+When i = 7, stack = [7 (73)]. 				ans[7] = 0.
+When i = 6, stack = [6 (76)]. 				ans[6] = 0.
+When i = 5, stack = [5 (72), 6 (76)]. 			ans[5] = 1.
+When i = 4, stack = [4 (69), 5 (72), 6 (76)]. 		ans[4] = 1.
+When i = 3, stack = [3 (71), 5 (72), 6 (76)]. 		ans[3] = 2.
+When i = 2, stack = [2 (75), 6 (76)]. 			ans[2] = 4.
+When i = 1, stack = [1 (74), 2 (75), 6 (76)]. 		ans[1] = 1.
 When i = 0, stack = [0 (73), 1 (74), 2 (75), 6 (76)]. 	ans[0] = 1.
+
 */
 
 class Solution {
