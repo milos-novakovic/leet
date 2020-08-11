95eb79ff4abd407d9f86c84c08017bf797c168cf
milos-novakovic <58906634+milos-novakovic@users.noreply.github.com>
2020-08-11 14:37:17 +0200

    Update dailyTemperatures.cpp

--- dailyTemperatures.cpp
+++ dailyTemperatures.cpp
@@ -6,9 +6,10 @@ If there is no future day for which this is possible, put 0 instead.
 For example, given the list of temperatures T = [73, 74, 75, 71, 69, 72, 76, 73], your output should be [1, 1, 4, 2, 1, 1, 0, 0].
 
 Note: The length of temperatures will be in the range [1, 30000]. Each temperature will be an integer in the range [30, 100].
-*/
 
-/*
+-----------------------------------SOLUTION------------------------------------------------
+
+
 (leetcode)
 Here is a worked example of the contents of the stack as we work through T = [73, 74, 75, 71, 69, 72, 76, 73]
  in reverse order, at the end of the loop (after we add T[i]). 
