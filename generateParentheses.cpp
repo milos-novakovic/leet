b5d618d03e690d8325d4037c37b8d0be84c70c45
Milos Novakovic <58906634+milos-novakovic@users.noreply.github.com>
2020-08-12 06:47:57 +0200

    Update generateParentheses.cpp

--- generateParentheses.cpp
+++ generateParentheses.cpp
@@ -3,17 +3,17 @@
 class Solution {
 public:
     vector<string>* res = nullptr;
-    int numberOfPairs = 0; 	// given number of total pairs of parentheses
+    int numOfPairs = 0; 	// given number of total pairs of parentheses
     void helper(string curr, int numOfOpen, int numOfClosed) {
         // numOfOpen := number of open parentheses '(' 
         // numOfClosed := number of close parentheses ')' 
         // curr := string that is going to be inserted in the resulting vector *p
-        if ((numOfOpen + numOfClosed) == 2 * numberOfPairs) {
+        if ((numOfOpen + numOfClosed) == 2 * numOfPairs) {
             // found one solution, remember it and return
             res->push_back(curr);
             return;
         }
-        if (numOfOpen == numberOfPairs) {
+        if (numOfOpen == numOfPairs) {
             // only room for close parentheses; example curr = "(((" and numOfOpen=3 numOfPairs=3
             helper(curr + ')', numOfOpen, numOfClosed + 1);
         }
@@ -28,8 +28,8 @@ public:
     }
     vector<string> generateParenthesis(int n) {
         res = new vector<string>;
-        numberOfPairs = n;
-        helper('(', 1, 0);  // always must start with open parentheses
+        numOfPairs = n;
+        helper("(", 1, 0);  // always must start with open parentheses
         return *res;
     }
     ~Solution()
